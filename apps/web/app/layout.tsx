70c12fe86801f0c478bfe4715327516d18688de2
Andrew Hernandez <ahernandez@fairsquare.com>
2026-02-25 19:39:32 -0800

    mycel: install Auth0 SDK, add provider and route handler
    - Install @auth0/nextjs-auth0@3.3.0 (compatible with Next.js 14.2.3)
    - Create dynamic catch-all route at /api/auth/[auth0] for login/logout/callback/me
    - Create Auth0Provider client component wrapping UserProvider
    - Wire Auth0Provider into root layout as outermost wrapper

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Auth0Provider } from "./auth0-provider";
 import { ThemeProvider } from "./lib/theme/context";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,9 +12,9 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        <Auth0Provider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </Auth0Provider>
       </body>
     </html>
   );
